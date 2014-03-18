d04fa252621fecfa3b5d5758d6f5ea188fe31d03
Todd Anderson <bustardcelly@gmail.com>
2014-03-18 09:55:33 -0400

    layout and style changes for web client.

--- massroute/__init__.py
+++ massroute/__init__.py
@@ -13,5 +13,11 @@ def predictions_list(predictions):
 def format_prediction(minutes, seconds):
   return minutes + ' minutes ' + str(int(seconds) % 60) + ' seconds' if int(minutes) > 0 else seconds + ' seconds'
 
+def format_prediction_breadcrumb(prediction):
+  breadcrumb = '/routes/' + prediction["routeTag"]
+  dir_tag = None if len(prediction["predictions"]) == 0 else prediction["predictions"][0]["dirTag"]
+  return breadcrumb if dir_tag == None else '%s/destinations/%s' % (breadcrumb, dir_tag)
+
+app.jinja_env.globals.update(format_prediction_breadcrumb=format_prediction_breadcrumb)
 app.jinja_env.globals.update(format_prediction=format_prediction)
 app.jinja_env.globals.update(predictions_list=predictions_list)
